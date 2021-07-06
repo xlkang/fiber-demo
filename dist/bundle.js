190df20cc6c831deffd7dc43f36a740e5123fb18
xinlin kang <xlkang@mail.ustc.edu.cn>
2021-07-06 20:10:17 +0800

    init

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("from client.js");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
